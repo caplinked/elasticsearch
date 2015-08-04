8461c025eaa72ade5b3afcdd92064a2b44be4ed0
Jimmy Trowbridge <trowbridge.james@gmail.com>
2015-08-04 15:27:16 -0400

    fix cron command

--- recipes/cron.rb
+++ recipes/cron.rb
@@ -7,6 +7,6 @@ cron 'cookbooks_report' do
     #create repository if it doesn't already exist
     "curl -XPUT 'http://localhost:9200/_snapshot/cl-es-backups' -d '{ \"type\": \"s3\", \"settings\": { \"bucket\": \"caplinked-elasticsearch-backups\", \"region\": \"us-west-2\" } }'",
     #create backup
-    "curl -XPUT 'http://localhost:9200/_snapshot/cl-es-backups/#{Time.now.to_i}?wait_for_completion=true"
+    "curl -XPUT 'http://localhost:9200/_snapshot/cl-es-backups/#{Time.now.to_i}?wait_for_completion=true'"
   ].join('; ')
 end
